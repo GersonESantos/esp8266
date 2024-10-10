ca5ccaf3ec81199f688ac1713d8d130695ccee9b
GersonESS <gersones@outlook.com>
2024-10-10 16:31:48 -0300

    Create sketch_oct10a.ino

---
+++ testes/sketch_oct10a/sketch_oct10a.ino
@@ -0,0 +1,18 @@
+// Define o pino onde o LED está conectado
+const int ledPin = 4;  // GPIO 4 corresponde ao pino D2 na ESP8266
+
+void setup() {
+  // Configura o pino do LED como saída
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // Liga o LED
+  digitalWrite(ledPin, HIGH);
+  // Aguarda 0,5 segundos
+  delay(500);
+  // Desliga o LED
+  digitalWrite(ledPin, LOW);
+  // Aguarda mais 0,5 segundos
+  delay(500);
+}
